8b32481da1656d88eae2f97909db82d854d86fd1
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-07-07 19:01:04 -0700

    updated javascript to store and place items properly

--- assets/script.js
+++ assets/script.js
@@ -10,10 +10,16 @@ var fiveTime = $("#5pm-time-display");
 var currentTime = $("#date-time");
 var btn = $("#button-1");
 var input = $("#user-input-1");
-var input2 = $("#user-input-2");
 var save = $("#button-1");
-var rowOne = $('#row-1')
-
+var rowOne = $('#row-1');
+var input2 = $('#user-input-2')
+var input3 = $('#user-input-3');
+var input4 = $('#user-input-4');
+var input5 = $("#user-input-5");
+var input6 = $("#user-input-6");
+var input7 = $("#user-input-7");
+var input8 = $("#user-input-8");
+var input9 = $("#user-input-9");
 function displayTime() {
     var rightNow = dayjs().format("dddd, MMM, d [at] hh:mm:ss a");
     currentTime.text(rightNow);
@@ -136,7 +142,7 @@ function isItTime8() {
         console.log("its 4");
     } else if (time > "4pm") {
         document.querySelector("#row-8").style.backgroundColor = "grey";
-        console.log("its past 48");
+        console.log("its past 8");
     } else {
         time < "4pm";
         document.querySelector("#row-8").style.backgroundColor = "green";
@@ -173,7 +179,6 @@ isItTime9();
 function get() {
     var storedItem = localStorage.getItem("storedItem");
     console.log("stored Item:", storedItem);
-    if (storedItem) {
         console.log("stored Item exists");
           storedItem = input.textContent;
          $("#row-1")
@@ -181,33 +186,31 @@ function get() {
             .text(localStorage.getItem("storedItem"));
         $("#row-2")
             .children("#user-input-2")
-            .text(localStorage.getItem("storedItem10am"));
+            .text(localStorage.getItem("storedItem2"));
         $("#row-3")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-3")
+            .text(localStorage.getItem("storedItem3"));
         $("#row-4")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-4")
+            .text(localStorage.getItem("storedItem4"));
         $("#row-5")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-5")
+            .text(localStorage.getItem("storedItem5"));
         $("#row-6")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-6")
+            .text(localStorage.getItem("storedItem6"));
         $("#row-7")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-7")
+            .text(localStorage.getItem("storedItem7"));
         $("#row-8")
-            .children("#user-input-1")
-            .text(localStorage.getItem("storedItem"));
+            .children("#user-input-8")
+            .text(localStorage.getItem("storedItem8"));
             $("#row-9")
-                .children("#user-input-1")
-                .text(localStorage.getItem("storedItem"));
+                .children("#user-input-9")
+                .text(localStorage.getItem("storedItem9"));
 
-    } else {
-        console.log("stored item not found");
-    }
-}
+    } 
+
 
 const button = document.querySelector("#button-1");
 button.addEventListener("click", function () {
@@ -223,6 +226,48 @@ button2.addEventListener("click", function () {
     input2.val(item2);
     console.log(item2);
 
+});
+const button3 = document.querySelector("#button-3");
+button3.addEventListener("click", function () {
+    var item3 = input3.val();
+    localStorage.setItem("storedItem3", item3);
+    input3.val(item3);
+});
+const button4 = document.querySelector("#button-4");
+button4.addEventListener("click", function () {
+    var item4 = input4.val();
+    localStorage.setItem("storedItem4", item4);
+    input4.val(item4);
+});
+const button5 = document.querySelector("#button-5");
+button5.addEventListener("click", function () {
+    var item5 = input5.val();
+    localStorage.setItem("storedItem5", item5);
+    input5.val(item5);5
+});
+const button6 = document.querySelector("#button-6");
+button6.addEventListener("click", function () {
+    var item6 = input6.val();
+    localStorage.setItem("storedItem6", item6);
+    input6.val(item6);
+});
+const button7 = document.querySelector("#button-7");
+button7.addEventListener("click", function () {
+    var item7 = input7.val();
+    localStorage.setItem("storedItem7", item7);
+    input7.val(item7);
+});
+const button8 = document.querySelector("#button-8");
+button8.addEventListener("click", function () {
+    var item8 = input8.val();
+    localStorage.setItem("storedItem8", item8);
+    input8.val(item8);
+});
+const button9 = document.querySelector("#button-9");
+button9.addEventListener("click", function () {
+    var item9 = input9.val();
+    localStorage.setItem("storedItem9", item9);
+    input9.val(item9);
 });
 
 get();
